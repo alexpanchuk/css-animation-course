f7c0a8b69ff5b2d3a5d410d15beaad9e08b373aa
Alex Panchuk <apanchuk@gmail.com>
2020-04-16 00:01:46 +0500

    Complete animating on scroll

---
+++ 05-on-scroll/javascripts/show-on-scroll.js
@@ -0,0 +1,31 @@
+const rafFallback = (cb) => setTimeout(cb, 100 / 60);
+const scroll = window.requestAnimationFrame || rafFallback;
+
+const elementsToShow = document.querySelectorAll(".show-on-scroll");
+
+const loop = () => {
+  elementsToShow.forEach((el) => {
+    if (isElementInViewport(el)) {
+      el.classList.add("is-visible");
+    } else if (el.classList.contains("is-visible")) {
+      el.classList.remove("is-visible");
+    }
+  });
+  scroll(loop);
+};
+
+loop();
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
